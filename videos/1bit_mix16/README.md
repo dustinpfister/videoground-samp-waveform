6836f4b3415dad189573fd5e95bf05bac2a86e63
dustinpfister <dustin.pfister@gmail.com>
2024-02-25 16:59:29 -0500

    first video for 1bit_mix16 started

--- videos/1bit_mix16/README.md
+++ videos/1bit_mix16/README.md
@@ -1,3 +1,3 @@
 # 1bit_mix16
 
-The general idea here is to have more than one 1bit sound track, however when it comes to creating a 16bit mix I am not merging down and then scaling, but rather scaling up to 16bit for each track. Then sense I have 16bit tracks I can then mix as usual.
+The general idea here is to have more than one 1bit track, however when it comes to creating a 16bit mix I am not merging down and then scaling. Alliteratively I am scaling up to 16bit for each track, then sense I have 16bit tracks I can then mix as usual. As such this results in having more then one 1bit track that is played in parallel.
---
+++ videos/1bit_mix16/videos/video01-01-test-2tracks.js
@@ -0,0 +1,105 @@
+/*    video01-01-test-2tracks - form 1bit_mix16 in videoground-samp-waveform 
+          * I just want to get the basic idea working
+ */
+//-------- ----------
+// SCRIPTS
+//-------- ----------
+VIDEO.scripts = [
+  '../../../js/samp_create/r0/samp_tools.js',
+  '../../../js/samp_create/r0/samp_create.js',
+  '../../../js/samp_create/r0/samp_draw.js',
+  '../../../js/samp_create/r0/waveforms/pulse.js',
+  '../../../js/samp_create/r0/waveforms/table_maxch.js'
+];
+//-------- ----------
+// INIT
+//-------- ----------
+VIDEO.init = function(sm, scene, camera){
+
+    const sud = scene.userData;
+    sm.renderer.setClearColor(0x000000, 0.25);
+
+    const TRACKS = [];
+
+    TRACKS[0] = [
+
+        0,0,1,0,2,0,3,0, 4,0,5,0,6,0,7,0, 8,0,0,0,0,0,0,0, 8,0,0,0,0,0,0,0,
+        0,0,1,0,2,0,3,0, 4,0,5,0,6,0,7,0, 8,0,0,0,0,0,0,0, 8,0,0,0,0,0,0,0,
+        0,0,1,0,2,0,3,0, 4,0,5,0,6,0,7,0, 8,0,0,0,0,0,0,0, 8,0,0,0,0,0,0,0,
+
+        0,0,2,0,3,0,4,0, 5,0,6,0,7,0,8,0, 9,0,0,0,0,0,0,0, 10,0,0,0,0,0,0,0,
+        0,0,2,0,3,0,4,0, 5,0,6,0,7,0,8,0, 9,0,0,0,0,0,0,0, 10,0,0,0,0,0,0,0,
+        0,0,2,0,3,0,4,0, 5,0,6,0,7,0,8,0, 9,0,0,0,0,0,0,0, 10,0,0,0,0,0,0,0,
+
+    ];
+
+    TRACKS[1] = [
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1
+    ];
+
+    const sound = sud.sound = CS.create_sound({
+        waveform : 'table_maxch',
+        for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
+
+
+            const ni0 = TRACKS[0][ Math.floor( TRACKS[0].length * a_sound2) ];
+            fs.freq0 = ST.notefreq_by_indices(3, ni0);
+            fs.amp0 = ni0 === 0 ? 0 : 0.5;
+
+            const ni1 = TRACKS[1][ Math.floor( TRACKS[1].length * a_sound2) ];
+            fs.freq1 = ST.notefreq_by_indices(1, ni1);
+            fs.amp1 = ni1 === 0 ? 0 : 0.5; 
+
+            return fs;
+
+        },
+        for_sampset: ( samp, i, a_sound, fs, opt ) => {
+
+            return {
+                frequency: 1,
+                amplitude: 1,
+                maxch: 2,
+                table: [
+                    { waveform: 'pulse', frequency: fs.freq0, amplitude: fs.amp0 },
+                    { waveform: 'pulse', frequency: fs.freq1, amplitude: fs.amp1 }
+                ]
+            }
+
+        },
+        secs: 6
+    });
+
+    sud.opt_frame = { w: 1200, h: 200, sy: 500, sx: 40, mode: sound.mode, overlay_alpha: 0.5 };
+    sm.frameMax = sound.frames;
+
+};
+//-------- ----------
+// UPDATE
+//-------- ----------
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const sud = scene.userData;
+    // create the data samples
+    const data_samples = CS.create_frame_samples(sud.sound, sm.frame, sm.frameMax );
+    return CS.write_frame_samples(sud.sound, data_samples, sm.frame, sm.imageFolder, sm.isExport);
+};
+//-------- ----------
+// RENDER
+//-------- ----------
+VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
+    const sud = scene.userData;
+    const sound = sud.sound;
+
+    // background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    // draw frame disp, and info
+    DSD.draw( ctx, sound.array_frame, sud.opt_frame, 0, 'final 16-bit mix' );
+    DSD.draw_info(ctx, sound, sm);
+};
+
