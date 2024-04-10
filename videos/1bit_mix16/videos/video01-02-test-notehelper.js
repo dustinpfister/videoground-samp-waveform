132d4f71bac34d57e5e057701f2a45e0530c8555
dustinpfister <dustin.pfister@gmail.com>
2024-04-10 08:28:45 -0400

    started a video01-02-test-notehelper video

---
+++ videos/1bit_mix16/videos/video01-02-test-notehelper.js
@@ -0,0 +1,125 @@
+/*    video01-02-test-notehelper - form 1bit_mix16 in videoground-samp-waveform 
+ */
+//-------- ----------
+// SCRIPTS
+//-------- ----------
+VIDEO.scripts = [
+  '../../../js/samp_create/r0/samp_tools.js',
+  '../../../js/samp_create/r0/samp_create.js',
+  '../js/bit_tracks/r0/bit_tracks.js',
+  '../js/bit_tracks/r0/bit_samp_draw.js',
+
+];
+//-------- ----------
+// INIT
+//-------- ----------
+VIDEO.init = function(sm, scene, camera){
+
+    const create_track_note = ( nums_per_sec=8, pitch=1, secs=1 ) => {
+        const nums = [];
+        let i = 0;
+        const len= Math.ceil( nums_per_sec * secs );
+        while(i < len ){
+            nums.push( pitch );
+            i += 1;
+        }
+        return nums;
+    };
+    
+    const song = [
+        [0, 1],
+        [1, 3],
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],       
+    ];
+    
+   let track = [];
+    song.forEach( (params) => {
+    
+    
+        
+        const arr = create_track_note(8, params[0], params[1]);
+    
+        track.push(arr)
+        
+    
+    });
+    
+    track = track.flat();
+    console.log(track.flat())
+    
+
+    const sud = scene.userData;
+    sm.renderer.setClearColor(0x000000, 0.25);
+
+    sud.tracks = Bit_tracks.create({
+        count: 1,
+        octives: [3, 1],
+        duty: [0.50, 0.30 ]
+    });
+
+    sud.tracks.notes = [[],[]];
+    
+    sud.tracks.desc = ['highs', 'lows'],
+
+    sud.tracks.notes[0] = track.flat();
+
+
+    // 1 bit track sample data arrays used for display
+    sud.array_frame_tracks = [];
+
+    // create the main sound object using CS.create_sound
+    const sound = sud.sound = CS.create_sound({
+        waveform: Bit_tracks.waveforms.mix,
+        for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
+            Bit_tracks.new_frame(sud.tracks, a_sound2);
+            return fs;
+        },
+        for_sampset: ( samp, i, a_sound, fs, opt ) => {
+            return Bit_tracks.for_sampset(sud.tracks, a_sound, opt.secs, 0.50 );
+        },
+        secs: 5
+    });
+
+    // display objects for audio sample arrays for tracks and main final display
+    sud.track_disp_opt = DSD.create_disp_options(sud.tracks, sound, { line_width: 3, midline_style: '#444400' });
+
+    // set vg sm.frameMax to frames value of sound object
+    sm.frameMax = sound.frames;
+
+};
+//-------- ----------
+// UPDATE
+//-------- ----------
+VIDEO.update = function(sm, scene, camera, per, bias){
+
+    const sud = scene.userData;
+
+    // create the data samples
+    const data_samples = CS.create_frame_samples(sud.sound, sm.frame, sm.frameMax );
+    return CS.write_frame_samples(sud.sound, data_samples, sm.frame, sm.imageFolder, sm.isExport);
+
+};
+//-------- ----------
+// RENDER
+//-------- ----------
+VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
+
+    const sud = scene.userData;
+    const sound = sud.sound;
+
+    // background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    // draw sample data for 1bit tracks, and 16bit mix
+    DSD.draw_tracks(ctx, sud.tracks, sud.track_disp_opt);
+    DSD.draw( ctx, sound.array_frame, sud.track_disp_opt.mix, sm.per, 'final 16-bit mix' );
+
+    // top display info
+    DSD.draw_info(ctx, sound, sm, '#ffffff', 30);
+
+};
+
