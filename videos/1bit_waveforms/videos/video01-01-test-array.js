c20694a5c486ea7ace4b14472c8647272ea4b7a1
dustinpfister <dustin.pfister@gmail.com>
2024-04-29 12:36:47 -0400

    seeded random helper for video01-01 of 1bit_waveforms project

--- videos/1bit_waveforms/videos/video01-01-test-array.js
+++ videos/1bit_waveforms/videos/video01-01-test-array.js
@@ -18,17 +18,47 @@ VIDEO.init = function(sm, scene, camera){
    
     const sud = scene.userData;
     sm.renderer.setClearColor(0x000000, 0.25);
+
+    const create_seedrnd_1bit = (count=32, seed_start=0, seed_denom=1, seed_delta=1 ) => {
+        let i = 0;
+        const array = [];
+        while(i < count){
+            const n = THREE.MathUtils.seededRandom( seed_start + Math.floor(i / seed_denom ) * seed_delta );
+            array.push( n >= 0.5 ? 1 : 0 );
+            i += 1;
+        }
+        return array;
+    };
+
+console.log(create_seedrnd_1bit());
+
+
+    const array_options = [
+        new Array(128).fill(0)
+        .concat( create_seedrnd_1bit(128, 0, 5, 1))
+        .concat( new Array(128).fill(1) )
+        .concat( create_seedrnd_1bit(256, 0, 1, 1) )
+        .concat( new Array(128).fill(0) )
+
+
+
+
+        //[ 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1 ],
+        //[ 0,0,0,0,0,0,0,1, 0,1,1,1,0,1,1,1, 1,0,1,1,1,1,1,0, 1,1,1,1,1,1,1,1 ],
+        //[ 0,0,0,0,1,1,1,1, 0,0,1,1,0,0,1,1, 1,0,1,0,1,0,1,0, 1,1,1,1,1,1,1,1 ],
+        //[ 0,1,0,1,1,1,0,1, 0,1,1,1,0,0,1,1, 1,0,1,0,1,0,1,0, 1,0,1,1,0,0,1,1 ],
+
+    ];
+
     
     // array abit waveform
     const array_1bit = (samp, a_wave) => {
-        samp.array = samp.array || [0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0];
+        samp.array = samp.array || [0,1,1,1,1,1,0,1,0,1];
         const len_array = samp.array.length;
         const a = a_wave * samp.frequency % 1 ;
         let i_array = Math.floor( len_array * a );
         const n = samp.array[i_array];
-        
-        
-        return Math.random();
+        return n;
     };
 
     // set up tracks object
@@ -50,6 +80,12 @@ VIDEO.init = function(sm, scene, camera){
     const sound = sud.sound = CS.create_sound({
         waveform: Bit_tracks.waveforms.mix,
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
+
+            const samp = sud.tracks.objects[0].samp;
+            const i_option = Math.floor( array_options.length * a_sound2 );
+            samp.array = array_options[i_option];
+            samp.frequency = 60;
+
             Bit_tracks.new_frame(sud.tracks, a_sound2);
             return fs;
         },
@@ -57,7 +93,7 @@ VIDEO.init = function(sm, scene, camera){
             const sec_alpha = Samp_alphas.cell(i, 44100, 0);
             return Bit_tracks.for_sampset(sud.tracks, a_sound, opt.secs, 0.35, sec_alpha );
         },
-        secs: 3
+        secs: 5
     });
     // display objects for audio sample arrays for tracks and main final display
     sud.track_disp_opt = DSD.create_disp_options(sud.tracks, sound, { line_width: 3, midline_style: '#444400' });
