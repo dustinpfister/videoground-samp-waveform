f5e37cd32cf88e0e27a57f97a38c7860bc339e4f
dustinpfister <dustin.pfister@gmail.com>
2024-05-29 07:30:38 -0400

    new look for nyquest_frequnecy worked out

--- videos/nyquist_frequency/videos/video01-01-pulse.js
+++ videos/nyquist_frequency/videos/video01-01-pulse.js
@@ -20,6 +20,7 @@ VIDEO.init = function(sm, scene, camera){
 
     sud.samples_per_cycle = 0;
     sud.frequency = 0;
+    sud.data_samples = [];
 
     const sound = sud.sound = CS.create_sound({
         waveform : (samp, a_wave) => {
@@ -28,9 +29,9 @@ VIDEO.init = function(sm, scene, camera){
              const cycle_points = Math.round( a_cycle * 100) % 100;
 
              if(cycle_points < 50){
-                 return samp.amplitude * 1;
+                 return -1 + samp.amplitude * 2;
              }
-             return samp.amplitude * -1;
+             return -1;
         },
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
             return fs;
@@ -42,14 +43,8 @@ VIDEO.init = function(sm, scene, camera){
 
             const spc = sud.samples_per_cycle = Math.floor(700 - 698 * a_sound) ;
             sud.frequency = samp.frequency = sample_rate / spc;
-            samp.amplitude = 0.40;
+            samp.amplitude = 1.00;
 
-/*
-            sud.frequency = samp.frequency = Math.floor(sample_rate / 2 * a_sound);
-            samp.amplitude = 0.40;
-
-            sud.samples_per_cycle = sample_rate / samp.frequency;
-*/
 
             if(i % 1470 === 0 ){
                 //console.log( i, sud.samples_per_cycle )
@@ -58,7 +53,7 @@ VIDEO.init = function(sm, scene, camera){
 
             return samp;
         },
-        secs: 300
+        secs: 60
     });
     sud.opt_frame = { w: 1200, h: 420, sy: 150, sx: 40, mode: sound.mode, overlay_alpha: 0.5 };
     sm.frameMax = sound.frames;
@@ -69,11 +64,9 @@ VIDEO.init = function(sm, scene, camera){
 VIDEO.update = function(sm, scene, camera, per, bias){
     const sud = scene.userData;
     // create the data samples
-    const data_samples = CS.create_frame_samples(sud.sound, sm.frame, sm.frameMax );
+    sud.data_samples = CS.create_frame_samples(sud.sound, sm.frame, sm.frameMax );
 
-//console.log(data_samples)
-
-    return CS.write_frame_samples(sud.sound, data_samples, sm.frame, sm.imageFolder, sm.isExport);
+    return CS.write_frame_samples(sud.sound, sud.data_samples, sm.frame, sm.imageFolder, sm.isExport);
 };
 //-------- ----------
 // RENDER
@@ -86,19 +79,58 @@ VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0, canvas.width, canvas.height);
 
-    // top text info
+    // text info - freq and samp per sec
     const text_info = '' + 
         ' frequency: ' + sud.frequency.toFixed(4) +
-        ' samples_per_cycle: ' + sud.samples_per_cycle.toFixed(4);
-        
+        ' samples_per_cycle: ' + sud.samples_per_cycle.toFixed(4);        
     ctx.fillStyle = 'white';
     ctx.font = '30px  courier';
     ctx.textBaseline = 'top';
     ctx.fillText(text_info,10,10);
 
+    // text info - freq and samp per sec
     const text_info2 = '' +
         ' frame: ' + String(sm.frame).padStart(String(sm.frameMax).length, '0') + '/' + sm.frameMax;
     ctx.fillText(text_info2, 10, 40);
+
+
+    // data samples
+
+    const len = sud.data_samples.length;
+
+    const i_delta = len / 10;
+
+    let i_slice = 0;
+    while(i_slice < 10){
+
+        const i_start = i_delta * i_slice;
+        const i_end = i_start + i_delta;
+        const slice1 = sud.data_samples.slice(i_start, i_end);
+
+        slice1.forEach( (sample, i) => {
+            const n = ST.mode_to_raw(sample, 'int16');
+            const a = 0.01 + 0.99 * (n + 1) / 2;
+
+            const x = 10 + (i * 8.5);
+            my = 120 + 60 * i_slice;
+
+            const sy = my - 25 * a;
+            const ey = my + 25 * a;
+
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = 'lime';
+
+            ctx.beginPath();
+            ctx.moveTo(x, sy);
+            ctx.lineTo(x, ey);
+            ctx.closePath();
+            ctx.stroke();
+        
+        });
+
+        i_slice += 1;
+    }
+
 
     // draw frame disp, and info
     //DSD.draw( ctx, sound.array_frame, sud.opt_frame, 0, 'sample data ( current frame )' );
